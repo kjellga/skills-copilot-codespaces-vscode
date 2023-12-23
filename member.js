22ff7a6fce9097f303247839551dcdcb064f6b23
Kjell Inge Gausland <kjellinge@kraftlauget.no>
2023-12-23 15:03:49 +0000

    Added member.js

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = ["HTML", "CSS", "JavaScript"];
+  var age = 28;
+  var name = "John";
+  var address = {
+    street: "123 Main St.",
+    city: "New York",
+    state: "NY"
+  };
+}
